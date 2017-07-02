2bba3ad7a46d213f788e4cc56cc7b8d258205c27
Walfie <walfington@gmail.com>
2017-07-02 16:54:08 -0400

    Fetch images using Hyper

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,83 @@
+#[macro_use]
+extern crate error_chain;
+
 extern crate image;
 extern crate img_hash;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+extern crate futures;
 
-fn main() {
-    // wget "https://pbs.twimg.com/media/CT6cDD3UkAEnP8Y.jpg:large" -O ygg_jp.jpg
-    // wget "https://pbs.twimg.com/media/CfuZgxLUkAArdGe.jpg:large" -O ygg_en.jpg
-    let hash1 = hash(include_bytes!("../ygg_jp.jpg"), "ygg_jp");
-    let hash2 = hash(include_bytes!("../ygg_en.jpg"), "ygg_en");
+use futures::{Future, IntoFuture, Stream};
+use hyper_tls::HttpsConnector;
+use img_hash::ImageHash;
+use tokio_core::reactor::Core;
 
-    assert_eq!(hash1.dist(&hash2), 0);
+mod error;
+
+use error::*;
+
+type Client = hyper::Client<HttpsConnector<hyper::client::HttpConnector>>;
+
+quick_main!(|| -> Result<()> {
+    let mut core = Core::new().chain_err(|| "failed to initialize core")?;
+    let handle = core.handle();
+    let client = hyper::Client::configure()
+        .connector(HttpsConnector::new(4, &handle).chain_err(|| "HTTPS error")?)
+        .build(&handle);
+
+    let ygg = compare(
+        &client,
+        "https://pbs.twimg.com/media/CT6cDD3UkAEnP8Y.jpg:large",
+        "https://pbs.twimg.com/media/CfuZgxLUkAArdGe.jpg:large",
+    );
+
+    assert_eq!(core.run(ygg).chain_err(|| "failed to compare")?, 0);
+
+    Ok(())
+});
+
+fn compare(client: &Client, url1: &str, url2: &str) -> Box<Future<Item = usize, Error = Error>> {
+    let hash1 = get_and_hash(client, url1);
+    let hash2 = get_and_hash(client, url2);
+
+    Box::new(hash1.join(hash2).map(|(h1, h2)| h1.dist(&h2)))
 }
 
-fn hash(bytes: &[u8], name: &str) -> img_hash::ImageHash {
+
+fn get_and_hash(client: &Client, url: &str) -> Box<Future<Item = ImageHash, Error = Error>> {
+    let url_string = url.to_string();
+
+    let result = url.parse()
+        .chain_err(|| "invalid URL")
+        .map(|u| client.get(u).then(|r| r.chain_err(|| "HTTP error")))
+        .into_future()
+        .flatten()
+        .and_then(|resp| {
+            resp.body().concat2().then(|r| {
+                r.chain_err(|| "failed to get body")
+            })
+        })
+        .and_then(|bytes| hash(&bytes).into_future())
+        .then(move |r| {
+            r.chain_err(|| format!("failed to get URL: `{}`", url_string))
+        });
+
+    Box::new(result)
+}
+
+fn hash(bytes: &[u8]) -> Result<ImageHash> {
     use image::GenericImage;
 
-    let mut img = image::load_from_memory(bytes).expect(name);
+    let mut img = image::load_from_memory(bytes).chain_err(
+        || "failed to load bytes",
+    )?;
     let (w, h) = img.dimensions();
     img.crop(0, 0, w, h * 3 / 4);
 
-    img_hash::ImageHash::hash(&img, 8, img_hash::HashType::Gradient)
+    Ok(img_hash::ImageHash::hash(
+        &img,
+        8,
+        img_hash::HashType::Gradient,
+    ))
 }
